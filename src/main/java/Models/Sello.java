780d8bbfe3435ceb885f1170cd61fa96c9162707
nquinbla <nquinbla@myuax.com>
2024-10-22 17:18:46 +0200

    Sello código

--- src/main/java/Models/Sello.java
+++ src/main/java/Models/Sello.java
@@ -1,4 +1,30 @@
 package Models;
 
-public class Sello {
+public class Sello extends ElementoColeccionable {
+    private double altura;
+    private double anchura;
+    private String imagen;
+    private EstadoConservacion estadoConservacion;
+
+    public Sello(String pais, String autoridadGobernante, int annus, double valor, String unidadMonetaria, int rareza, double precio,
+                 double altura, double anchura, String imagen, EstadoConservacion estadoConservacion) throws IllegalArgumentException {
+        super(pais, autoridadGobernante, annus, valor, unidadMonetaria, rareza, precio);
+        this.altura = altura;
+        this.anchura = anchura;
+        this.imagen = imagen;
+        this.estadoConservacion = estadoConservacion;
+    }
+
+    // Getters y Setters
+    public double getAltura() { return altura; }
+    public void setAltura(double altura) { this.altura = altura; }
+
+    public double getAnchura() { return anchura; }
+    public void setAnchura(double anchura) { this.anchura = anchura; }
+
+    public String getImagen() { return imagen; }
+    public void setImagen(String imagen) { this.imagen = imagen; }
+
+    public EstadoConservacion getEstadoConservacion() { return estadoConservacion; }
+    public void setEstadoConservacion(EstadoConservacion estadoConservacion) { this.estadoConservacion = estadoConservacion; }
 }
