8d8eb0c14a7d104b5524d9f6c2ebb1b87559c8dd
nquinbla <nquinbla@myuax.com>
2024-10-22 18:25:53 +0200

    comentarios del EstadoConservacion

--- src/main/java/Models/EstadoConservacion.java
+++ src/main/java/Models/EstadoConservacion.java
@@ -3,3 +3,12 @@ package Models;
 public enum EstadoConservacion {
     G, VG, F, VF, XF, AU, UNC
 }
+/* Los estados de conservación de la moneda significan lo siguiente:
+G -> Good (Bueno)
+VG -> Very Good (Muy Bueno)
+F -> Fine (Bien)
+VF -> Very Fine (Muy Bien)
+XF -> Extra Fine (Extra Bien)
+AU -> Almost Uncirculated (Casi sin circular)
+UNC -> Uncirculated (Sin circular)
+ */
