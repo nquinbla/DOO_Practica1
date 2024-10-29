a9c678a7f7f56e0276b610d4745d967f37e651f2
nquinbla <nquinbla@myuax.com>
2024-10-29 17:26:17 +0100

    AleacionesValidas comentarios

--- src/main/java/ModelsAditivos/AleacionesValidas.java
+++ src/main/java/ModelsAditivos/AleacionesValidas.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AleacionesValidas {
+    // Lista de aleaciones válidas, inicializada con valores específicos
     private static final List<String> aleacionesValidas = Arrays.asList("Oro", "Plata", "Cobre", "Níquel", "Bronce");
 
+    // Método estático que verifica si una aleación es válida
     public static boolean esAleacionValida(String aleacion) {
+        // Retorna true si la aleación está en la lista de aleaciones válidas, de lo contrario false
         return aleacionesValidas.contains(aleacion);
     }
 }
