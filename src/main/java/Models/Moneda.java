1218e1e8894e954f74fa15214c9ff7223994bbb3
nquinbla <nquinbla@myuax.com>
2024-10-29 17:20:25 +0100

    Moneda comentarios

--- src/main/java/Models/Moneda.java
+++ src/main/java/Models/Moneda.java
@@ -1,16 +1,21 @@
 package Models;
 
+// Clase Moneda que extiende ElementoColeccionable -> representa una moneda con atributos adicionales específicos de su naturaleza
 public class Moneda extends ElementoColeccionable {
-    // [1] Atributos de la clase Moneda
-    private String composicion; // Aleación de la moneda
-    private double peso; // Peso de la moneda en gramos
-    private double diametro; // Diámetro de la moneda en milímetros
-    private double grosor; // Grosor de la moneda en milímetros
-    private EstadoConservacion estadoConservacion; // Estado de conservación de la moneda
+    // [1] Atributos de la clase Moneda -> Definen las características específicas de una moneda coleccionable
+    private String composicion;                                              // Composición o aleación de la moneda (ej., oro, plata, cobre)
+    private double peso;                                                          // Peso de la moneda en gramos
+    private double diametro;                                                  // Diámetro de la moneda en milímetros
+    private double grosor;                                                      // Grosor de la moneda en milímetros
+    private EstadoConservacion estadoConservacion;  // Estado de conservación de la moneda (ej., excelente, buena, regular)
 
-    // [2] Constructor de la clase Moneda
-    public Moneda(String pais, String autoridadGobernante, int annus, double valor, String unidadMonetaria, int rareza, double precio, String composicion, double peso, double diametro, double grosor, EstadoConservacion estadoConservacion) throws IllegalArgumentException {
+    // [2] Constructor de la clase Moneda -> Inicializa los atributos de Moneda y hereda los atributos de ElementoColeccionable
+    public Moneda(String pais, String autoridadGobernante, int annus, double valor, String unidadMonetaria, int rareza, double precio,
+                  String composicion, double peso, double diametro, double grosor, EstadoConservacion estadoConservacion) throws IllegalArgumentException {
+        // Llama al constructor de la clase padre (ElementoColeccionable) para inicializar los atributos heredados
         super(pais, autoridadGobernante, annus, valor, unidadMonetaria, rareza, precio);
+
+        // Inicialización de los atributos específicos de Moneda
         this.composicion = composicion;
         this.peso = peso;
         this.diametro = diametro;
@@ -18,17 +23,19 @@ public class Moneda extends ElementoColeccionable {
         this.estadoConservacion = estadoConservacion;
     }
 
-    // [3] Getters y Setters
-    public String getComposicion() {return composicion;}
-    public double getPeso() {return peso;}
-    public double getDiametro() {return diametro;}
-    public double getGrosor() {return grosor;}
-    public EstadoConservacion getEstadoConservacion() {return estadoConservacion;}
+    // [3] Métodos de la clase Moneda -> Getters y Setters para acceder y modificar los atributos específicos de Moneda
 
-    public void setComposicion(String composicion) {this.composicion = composicion;}
-    public void setPeso(double peso) {this.peso = peso;}
-    public void setDiametro(double diametro) {this.diametro = diametro;}
-    public void setGrosor(double grosor) {this.grosor = grosor;}
-    public void setEstadoConservacion(EstadoConservacion estadoConservacion) {this.estadoConservacion = estadoConservacion;}
+    // Métodos "get" para acceder a los atributos de Moneda
+    public String getComposicion() { return composicion; }
+    public double getPeso() { return peso; }
+    public double getDiametro() { return diametro; }
+    public double getGrosor() { return grosor; }
+    public EstadoConservacion getEstadoConservacion() { return estadoConservacion; }
 
+    // Métodos "set" para modificar los valores de los atributos de Moneda
+    public void setComposicion(String composicion) { this.composicion = composicion; }
+    public void setPeso(double peso) { this.peso = peso; }
+    public void setDiametro(double diametro) { this.diametro = diametro; }
+    public void setGrosor(double grosor) { this.grosor = grosor; }
+    public void setEstadoConservacion(EstadoConservacion estadoConservacion) { this.estadoConservacion = estadoConservacion; }
 }
